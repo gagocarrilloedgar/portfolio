b4e74c8a2f3785d6b4611ae610f4da6dd3a974ef
Edgar <edgargc.upc@gmail.com>
2022-03-26 22:20:35 +0100

    fix header

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -4,13 +4,46 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './CustomFooter.js'
-import Header from './Header'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
-      <Header />
-      <main className="mb-auto">{children}</main>
+      <header className="flex items-center justify-between py-10">
+        <div>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div className="mr-3">
+                <Logo />
+              </div>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden lg:block">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="link-underline p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+      ƒ<main className="mb-auto">{children}</main>
       <Footer />
     </SectionContainer>
   )
